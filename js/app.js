c8f86f5e0bce85da5ea24153184340af6acdc4b7
johnantoni <john@johnantoni.com>
2010-12-08 19:18:31 +0000

    periodic

---
+++ js/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+	// get json elements
+	// get json chemicals
+	
+	// render elements as ([a] [figure])
+	// ..links jump to wikipedia
+	// ..use elements extra data to add class (color, extra details)
+	
+	// render chemicals as table
+	// ..use extra data as before
+
+	// render cleanly
+
+});
